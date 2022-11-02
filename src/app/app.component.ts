8bbe30fb76ce90800d98ed1a13daa45ed1aa24ba
Diego Lobo <90005367@pmenos.com.br>
2022-11-01 22:52:53 -0300

    Incluindo componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Download } from './download/download';
+import { DownloadService } from './download/download.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'download-poc';
+  slides =
+    {
+      name: 'Mobile Cross-Platform from a Progressive Perspective',
+      url: 'https://www.historia.uff.br/stricto/td/1453.pdf'
+    };
+
+  download$!: Observable<Download>
+
+  constructor(private downloads: DownloadService,
+              @Inject(DOCUMENT) private document: Document) {}
+
+  download({name, url}: {name: string, url: string}) {
+    this.download$ = this.downloads.download(url, name)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { getSaver, SAVER } from './download/saver.provider';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [{provide: SAVER, useFactory: getSaver}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
